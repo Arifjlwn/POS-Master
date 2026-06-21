9218256a90041dfd6468012dfd94396f5eddfe57
Arif Juliawan <arifjuliawan123@gmail.com>
2026-06-22 01:18:27 +0700

    Ubah Landing Page

--- pos-frontend/src/views/public/LandingPage.vue
+++ pos-frontend/src/views/public/LandingPage.vue
@@ -66,7 +66,7 @@ const industries = [
 		title: 'Layanan Jasa (Cooming Soon)',
 		desc: 'Laundry, Barbershop, Bengkel, Steam Kendaraan',
 		icon: 'M21 16V8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16z M3.27 6.96L12 12.01l8.73-5.05',
-		isReady: true,
+		isReady: false,
 	},
 ];
 
